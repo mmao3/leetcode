169c10c497a2f6640af9b908adb0ffc658e915cd
MAO MAO <mmao3@binhamton.edu>
2019-03-18 21:22:21 -0400

    3/18

--- code/java/Solution_482.java
+++ code/java/Solution_482.java
@@ -1,1 +1,12 @@
-Solution_482.java
+class Solution_482 {
+    public String licenseKeyFormatting(String S, int K) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = S.length() - 1; i >= 0; i--) {
+            char c = S.charAt(i);
+            if (c != '-') {
+                sb.append(sb.length() % (K + 1) == K ? '-' : "").append(Character.toUpperCase(c));
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
